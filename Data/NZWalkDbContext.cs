2e893c5c62ba68dec90ec98803ae48d8541c214d
selianordGroup <selemanilusu@gmail.com>
2023-10-15 13:37:01 +0200

    Add the DbContext to the Project

---
+++ Data/NZWalkDbContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ctutrainingsolutions.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ctutrainingsolutions.Data
+{
+    public class NZWalkDbContext :DbContext
+    {
+       public NZWalkDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+       {
+        
+       }
+
+       //Add DbSet collection here 
+
+       public DbSet<Difficulty> difficulties { get; set; }
+
+       public DbSet<Region> Regions { get; set; }
+    }
+}
