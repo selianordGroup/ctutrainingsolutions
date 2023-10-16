7276c24c93b69239f895bef2e625100485f7e676
selianordGroup <selemanilusu@gmail.com>
2023-10-16 10:07:05 +0200

    add new class called Parents and Learner by Wait

---
+++ Models/Domain/Parent.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ctutrainingsolutions.Models.Domain
+{
+    public class Parent
+    {
+        public Guid ParentId { get; set; }
+        public string ParentName { get; set; }
+        public string ParentSurname { get; set; }
+        public string Email { get; set; }
+        public string CellphoneNumber { get; set; }
+        public string? LearnerImage { get; set; }
+
+        //Links tables
+        public Guid LearnerId { get; set; }
+
+        //Navigation table
+        public Learner Learner { get; set; }
+    }
+}
